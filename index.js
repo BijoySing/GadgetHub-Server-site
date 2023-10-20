ee93905c3fd4d836460d0c235fd3c371031f80fa
BijoySing <bijoysingh573@gmail.com>
2023-10-20 11:02:00 +0600

    database connect successfully

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const app =express();
-const port =process.env.PORT || 5000;
 require('dotenv').config();
+
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -12,47 +13,27 @@ app.use(express.json());
 
 
 
-console.log()
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5zf6vj.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 
-
-
-
-const uri = `mongodb+srv://{process.env.Db_USER}:{process.env.Db_PASS}@cluster0.g5zf6vj.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+const dbConnect = async () => {
+    try {
+        await client.connect()
+        console.log('Database Connected!')
+    } catch (error) {
+        console.log(error.name, error.message)
+    }
 }
-run().catch(console.dir);
-
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('running');
-})
-
-app.listen(port,()=>{
-    console.log(`running port:${port}`)
-})
+dbConnect()
